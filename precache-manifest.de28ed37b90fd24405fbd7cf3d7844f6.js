c060e173f64706571153b5a9068426b7e7eef588
Shamee1015 <mrimmoaee@gmail.com>
2020-01-13 16:10:36 +0600

    Updates

---
+++ precache-manifest.de28ed37b90fd24405fbd7cf3d7844f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53686f4149fa6b958730f552ac9b8e19",
+    "url": "/CRUD-APP-With-React-Hook/index.html"
+  },
+  {
+    "revision": "4304c10fd3c888943bbe",
+    "url": "/CRUD-APP-With-React-Hook/static/css/main.08011531.chunk.css"
+  },
+  {
+    "revision": "1c2fec4ed8d8f7319d72",
+    "url": "/CRUD-APP-With-React-Hook/static/js/2.78cd4b7f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/CRUD-APP-With-React-Hook/static/js/2.78cd4b7f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4304c10fd3c888943bbe",
+    "url": "/CRUD-APP-With-React-Hook/static/js/main.b356518a.chunk.js"
+  },
+  {
+    "revision": "73f0e57521a016b2d057",
+    "url": "/CRUD-APP-With-React-Hook/static/js/runtime-main.11a16654.js"
+  }
+]);
